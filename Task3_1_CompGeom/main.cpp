efedc6e4d142739e4896955e16d3781939dc0722
Paul <rekongstor@mail.ru>
2020-05-19 11:00:22 +0300

    Bugged implementation of Delaunay

--- Task3_1_CompGeom/main.cpp
+++ Task3_1_CompGeom/main.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <stack>
+#include <list>
 
 #define N 100
 #define SIZE_X 100
@@ -39,33 +40,58 @@ inline bool Left(Vertex v0, Vertex v1, Vertex v2)
    return Area2(v0, v1, v2) > 0;
 }
 
-struct Edge
+struct EdgeAdj;
+
+struct Triangle
 {
-   Vertex* v0;
-   Vertex* v1;
+   EdgeAdj* e0;
+   EdgeAdj* e1;
+   EdgeAdj* e2;
 };
 
 struct EdgeAdj
 {
-   Vertex* v0; // main
-   Vertex* v1; // adj
-   Vertex* v2; // main
-   Vertex* v3; // adj
+   Vertex* v0;
+   Vertex* v1;
+   Triangle* left;
+   Triangle* right;
 
-   EdgeAdj(Vertex* v0, Vertex* v1, Vertex* v2) : v0(v0), v1(v1), v2(v2)
-   {
-   }
-
-   void Add(Vertex V3)
-   {
-   }
 
    void CheckAndFlip()
    {
-      Vertex V0 = *v0;
+      // Edge
       Vertex V1 = *v1;
-      Vertex V2 = *v2;
-      Vertex V3 = *v3;
+      Vertex V3 = *v0;
+
+      // Left triangle
+      Vertex* lv0 = left->e0->v0;
+      Vertex* lv1 = left->e0->v1;
+      Vertex* lv2 = (left->e1->v0 != lv0 && left->e1->v0 != lv1) ? left->e1->v0 : left->e1->v1;
+
+      Vertex* lVertex;
+      if (lv0 != v0 && lv0 != v1)
+         lVertex = lv0;
+      else if (lv1 != v0 && lv1 != v1)
+         lVertex = lv1;
+      else
+         lVertex = lv2;
+
+      // Right triangle
+      Vertex* rv0 = right->e0->v0;
+      Vertex* rv1 = right->e0->v1;
+      Vertex* rv2 = (right->e1->v0 != rv0 && right->e1->v0 != rv1) ? right->e1->v0 : right->e1->v1;
+
+      Vertex* rVertex;
+      if (rv0 != v0 && rv0 != v1)
+         rVertex = rv0;
+      else if (rv1 != v0 && rv1 != v1)
+         rVertex = rv1;
+      else
+         rVertex = rv2;
+
+      Vertex V0 = *lVertex;
+      Vertex V2 = *rVertex;
+
       if (
          ((V0.x - V1.x) * (V0.y - V3.y) - (V0.x - V3.x) * (V0.y - V1.y)) *
          ((V2.x - V1.x) * (V2.x - V3.x) + (V2.y - V1.y) * (V2.y - V3.y))
@@ -74,25 +100,137 @@ struct EdgeAdj
          ((V2.x - V1.x) * (V2.y - V3.y) - (V2.x - V3.x) * (V2.y - V1.y))
          < 0.f)
       {
-         std::swap(v0, v1);
-         std::swap(v2, v3);
+         // Flip this edge
+         v0 = lVertex;
+         v1 = rVertex;
+
+         // Flip triangles
+         EdgeAdj *le1, *le2;
+         if (left->e0 == this)
+         {
+            le1 = left->e1;
+            le2 = left->e2;
+         }
+         else if (left->e1 == this)
+         {
+            le1 = left->e2;
+            le2 = left->e0;
+         } else
+         {
+            le1 = left->e0;
+            le2 = left->e1;
+         }
+
+         EdgeAdj* re1, * re2;
+         if (right->e0 == this)
+         {
+            re1 = right->e1;
+            re2 = right->e2;
+         }
+         else if (right->e1 == this)
+         {
+            re1 = right->e2;
+            re2 = right->e0;
+         }
+         else
+         {
+            re1 = right->e0;
+            re2 = right->e1;
+         }
+
+         left->e0 = this;
+         left->e1 = re2;
+         left->e2 = le1;
+
+         right->e0 = this;
+         right->e1 = le2;
+         right->e2 = re1;
       }
    }
 };
 
 
-void Delaunay(std::vector<Vertex>& vertices, std::vector<EdgeAdj> edges)
+void Delaunay(std::vector<Vertex>& vertices, std::list<EdgeAdj>& edges)
 {
    std::sort(vertices.begin(), vertices.end(), [](const Vertex& a, const Vertex& b) -> bool
    {
       return a.x < b.x;
    });
-   edges.emplace_back(EdgeAdj(&vertices[0], &vertices[1], &vertices[2]));
-   edges.emplace_back(EdgeAdj(&vertices[1], &vertices[2], &vertices[0]));
-   edges.emplace_back(EdgeAdj(&vertices[2], &vertices[0], &vertices[1]));
+   std::list<Triangle> triangles;
+   std::list<EdgeAdj*> hull;
+
+   // Adding one triangle, its edges and its convex hull
+   if (Left(vertices[0], vertices[1], vertices[2]))
+   {
+      triangles.push_back({});
+      edges.push_back({&vertices[0], &vertices[1], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e0 = &edges.back();
+      edges.push_back({&vertices[1], &vertices[2], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e1 = &edges.back();
+      edges.push_back({&vertices[2], &vertices[0], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e2 = &edges.back();
+   }
+   else
+   {
+      triangles.push_back({});
+      edges.push_back({&vertices[1], &vertices[0], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e0 = &edges.back();
+      edges.push_back({&vertices[0], &vertices[2], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e1 = &edges.back();
+      edges.push_back({&vertices[2], &vertices[1], &triangles.back()});
+      hull.push_back(&edges.back());
+      triangles.front().e2 = &edges.back();
+   }
+
+   for (int i = 3; i < vertices.size(); ++i)
+   {
+      // Finding visible start and end
+      auto start = hull.begin();
+      bool counterClockwise = Left(*(*start)->v0, *(*start)->v1, vertices[i]);
+      do
+      {
+         if (counterClockwise)
+         {
+            ++start;
+            if (!Left(*(*start)->v0, *(*start)->v1, vertices[i]))
+               break;
+         }
+         else
+         {
+            if (!Left(*(*start)->v0, *(*start)->v1, vertices[i]))
+               break;
+            --start;
+         }
+      }
+      while (start != hull.begin());
+
+      auto end = counterClockwise ? start : hull.begin();
+      while (!Left(*(*end)->v0, *(*end)->v1, vertices[i]))
+      {
+         ++end;
+      }
+
+      // Adding vertices to visible edges
+      for (auto it = start; it != end; ++it)
+      {
+         auto& edge = *it;
+         triangles.push_back({edge});
+         edge->right = &triangles.back();
+         edges.push_back({edge->v0, &vertices[i], &triangles.back()});
+         triangles.back().e1 = &edges.back();
+         edges.push_back({&vertices[i], edge->v1, &triangles.back()});
+         triangles.back().e2 = &edges.back();
+         edge->CheckAndFlip();
+      }
+   }
 }
 
-void Graham(std::vector<Vertex>& vertices, std::vector<EdgeAdj> edges)
+void Graham(std::vector<Vertex>& vertices, std::stack<Vertex*>& hullStack)
 {
    // Finding rightmost lowest point O(n)
    Vertex* plowRight = &vertices.front();
@@ -132,7 +270,6 @@ void Graham(std::vector<Vertex>& vertices, std::vector<EdgeAdj> edges)
    }
 
    // Initializing stack
-   std::stack<Vertex*> hullStack;
    hullStack.push(&vertices.front());
    hullStack.push(sortedVertices.front());
    int i = 2;
@@ -154,7 +291,6 @@ void Graham(std::vector<Vertex>& vertices, std::vector<EdgeAdj> edges)
          ++i;
       }
    }
-   return;
 }
 
 
@@ -169,11 +305,11 @@ int main()
 
 
    // Step 1: Convex hull
-   std::vector<EdgeAdj> edgesHull;
-   Graham(vertices, edgesHull);
+   std::stack<Vertex*> hullStack;
+   Graham(vertices, hullStack);
 
    // Step 2: Delaunay
-   std::vector<EdgeAdj> edgesTriangulation;
+   std::list<EdgeAdj> edgesTriangulation;
    Delaunay(vertices, edgesTriangulation);
 
    // Step 3: BMP
